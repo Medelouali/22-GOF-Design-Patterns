edf86562c1feae82325fdb5c29f113f553bab3f6
Medelouali <Mohammedelouali@gmail.com>
2023-03-21 16:11:30 +0000

    Adding diagrams

--- src/main/java/org/example/designPatterns/factory/DragonEnemyShip.java
+++
@@ -1,8 +0,0 @@
-package org.example.designPatterns.factory;
-
-public class DragonEnemyShip extends EnemyShip{
-
-    public DragonEnemyShip() {
-        setName("DragonEnemyShip");
-    }
-}
--- src/main/java/org/example/designPatterns/factory/UFOEnemyShip.java
+++
@@ -1,7 +0,0 @@
-package org.example.designPatterns.factory;
-
-public class UFOEnemyShip extends EnemyShip{
-    public UFOEnemyShip() {
-        setName("UFOEnemyShip");
-    }
-}
